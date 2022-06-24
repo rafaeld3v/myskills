b314d8e2599ed053f2bd4bb6642d79a5f8dfa3c6
rodrigo <rodrigo.goncalves@rocketseat.team>
2022-06-24 18:06:08 -0300

    fix: splash screen

--- android/app/src/main/java/com/myskills/MainActivity.java
+++ android/app/src/main/java/com/myskills/MainActivity.java
@@ -1,14 +1,21 @@
 package com.myskills;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import org.devio.rn.splashscreen.SplashScreen;
+
+import android.os.Bundle; // adicionado
+import org.devio.rn.splashscreen.SplashScreen; // adicionado
 
 public class MainActivity extends ReactActivity {
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
+  protected String getMainComponentName() {
+    return "myskills";
   }
+
+		// adicionado
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  
+        super.onCreate(savedInstanceState);
+    }
 }
